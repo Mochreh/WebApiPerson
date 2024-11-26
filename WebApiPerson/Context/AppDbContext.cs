7b032ea369adf06923225d1843311b2182b01402
Milagros Ruiz <milagrosruizp@outlook.com>
2024-11-25 21:07:04 -0500

    Add project files.

---
+++ WebApiPerson/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiPerson.Models;
+
+namespace WebApiPerson.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Person>().ToTable("Person");
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
